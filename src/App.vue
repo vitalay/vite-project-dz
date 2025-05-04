696d208fc4670890610b95ed39d46d1236c3fcac
vitalya <vitalay.efimov@gmail.com>
2025-05-04 17:19:36 +0500

    первый commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <button class="btn">Начать игру</button>
+  </main>
+</template>
+
+<style scoped>
+.btn {
+  background: var(--color-bg);
+  color: var(--color-cl);
+  width: 335px;
+  height: 68px;
+
+  border-radius: 100px;
+  gap: 10px;
+  font-size: 24px;
+  border: none;
+  cursor: pointer;
+  font-weight: 400;
+}
+</style>
